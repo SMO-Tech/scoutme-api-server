e5ed63b445cdf78a6de4c5c78938aa8dd843c34d
go-utsav <utsavgohel3@gmail.com>
2025-12-31 12:27:54 +0000

    updated cors for both origin

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ const app = express();
 app.use(
   cors({
     origin: [
-      ...(process.env.FRONTEND_URL ? [process.env.FRONTEND_URL] : []),
+      process.env.FRONTEND_URL!,
       "http://localhost:3000",
     ],
     credentials: true,
